2ab9ae375e7583aff4d8a70b12cb41767b590843
Gordon B. Isnor <gordon@gordon-b-isnors-macbook-pro.local>
2009-12-16 22:12:02 -0400

    working

--- stoptoad.rb
+++ stoptoad.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'rest_client'
+require 'crack'
 
 get '/' do
   erb :index 
@@ -12,21 +13,28 @@ post '/resolve' do
 end
 
 def resolve_all
-  errors = RestClient.get("http://#{@site}.hoptoadapp.com/errors/?auth_token=#{@key}")
-  unless errors.blank?
-    errors.each do |e|
-      resolve e[:id] 
+  url = "http://#{@site}.hoptoadapp.com/errors?auth_token=#{@key}" 
+  errors = RestClient.get(url)
+  errors = Crack::XML.parse(errors)
+  unless !defined?(errors["groups"]) || errors["groups"].nil? || errors["groups"].empty?
+    errors["groups"].each do |e|
+      "e is a #{e.class}"
+      puts e.inspect
+      resolve e["id"] 
     end 
     resolve_all
   else
-    "Stoptoad Complete!"
+    "Stoptoad Complete! <p><a href='/'>Back</a></p>"
   end
 end
 
 # Used by resolve_all to resolve a single error
 def resolve id    
+  puts "this is the id: #{id}"
   RestClient.put("http://#{@site}.hoptoadapp.com/errors/#{id}?auth_token=#{@key}", :group => { 
     :resolved => true
-    })
+    },
+    :auth_token => @key
+    )
 end
   
