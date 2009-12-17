ca16dd1a54004f52251e2a6271358f15bd3f7527
Gordon B. Isnor <gordon@gordon-b-isnors-macbook-pro.local>
2009-12-16 21:00:19 -0400

    test

--- stoptoad.rb
+++ stoptoad.rb
@@ -1,6 +1,35 @@
 require 'rubygems'
 require 'sinatra'
+require 'rest_client'
 
 get '/' do
-  "Hello from Sinatra on Heroku!"
+  erb :index 
+end
+
+get '/resolve' do
+  @key, @site = [params[:key], params[:site]]
+  resolve_all
+end
+
+def resolve_all
+  errors = RestClient.get("http://#{@site}.hoptoadapp.com/errors/?auth_token=#{@key}")
+  unless errors.blank?
+    errors.each do |e|
+      resolve e[:id] 
+    end 
+    resolve_all
+  else
+    "Stoptoad Complete!"
+  end
+end
+
+# Used by resolve_all to resolve a single error
+def resolve id    
+  puts "resolving error #{id}"
+  RestClient.put("http://#{@site}.hoptoadapp.com/errors/#{id}?auth_token=#{@key}", :group => { 
+    :resolved => true
+    })
+  end
+end
+  
 end
